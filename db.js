b026739d5218be48714f29ff7621de35b0a1e53e
Deepak Patidar <patidardeepak350@gmail.com>
2024-09-28 15:35:13 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,7 +1,5 @@
 const mongoose = require('mongoose');
 
-const mongoUrl = 'mongodb://127.0.0.1:27017/hotels';
-
 mongoose.connect(mongoUrl,{
     useNewUrlParser: true,
     useUnifiedTopology: true
